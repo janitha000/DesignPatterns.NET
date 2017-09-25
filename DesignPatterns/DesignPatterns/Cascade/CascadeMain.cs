1b5ce61a5a780eee1fa04e7c7acbb07bc0467384
Janitha Tennakoon <janitha000@gmail.com>
2017-09-25 17:18:40 +0530

    Added Cascade Pattern

---
+++ DesignPatterns/DesignPatterns/Cascade/CascadeMain.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Cascade
+{
+    class CascadeMain
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+
+            person.Talk();
+            person.Walk();
+            person.Sleep();
+            person.Jump();
+
+            new Person().Talk().Walk().Sleep().Jump();
+        }
+    }
+}
---
+++ DesignPatterns/DesignPatterns/Cascade/Person.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Cascade
+{
+    class Person
+    {
+        //public void Talk()
+        //{
+        //    Console.WriteLine("Hello");
+        //}
+
+        //public void Sleep()
+        //{
+        //    Console.WriteLine("zzzzzzzzzz");
+        //}
+
+        //public void Walk()
+        //{
+        //    Console.WriteLine("1 2 3 ");
+        //}
+
+        //public void Jump()
+        //{
+        //    Console.WriteLine("Higerrrrrrrr");
+        //}
+
+        public Person Talk()
+        {
+            Console.WriteLine("Hello");
+            return this;
+        }
+
+        public Person Sleep()
+        {
+            Console.WriteLine("Hello");
+            return this;
+        }
+
+        public Person Walk()
+        {
+            Console.WriteLine("Hello");
+            return this;
+        }
+
+        public Person Jump()
+        {
+            Console.WriteLine("Hello");
+            return this;
+        }
+    }
+}
