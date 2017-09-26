3b1ca6bf02d6372ccd76b8aef8b627cf258d7172
Janitha Tennakoon <janitha000@gmail.com>
2017-09-26 08:28:27 +0530

    Added pluggable Pattern

---
+++ DesignPatterns/DesignPatterns/Pluggable/PluggableMain.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Pluggable
+{
+    class PluggableMain
+    {
+        static void Main(string[] args)
+        {
+            GetTotal();
+            GetEvenTotal();
+            GetOddTotal();
+
+            GetTotal(i => true);
+            GetTotal(i => i % 2 == 0);
+            GetTotal(i => i % 2 == 1);
+
+        }
+
+        public static void GetTotal()
+        {
+            int total = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                total += i;
+            }
+            Console.WriteLine("total" + total);
+        }
+
+        public static void GetEvenTotal()
+        {
+            int total = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                if (i % 2 == 0)
+                    total += i;
+            }
+            Console.WriteLine("total" + total);
+        }
+
+        public static void GetOddTotal()
+        {
+            int total = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                if (i % 2 == 1)
+                    total += i;
+            }
+            Console.WriteLine("total" + total);
+        }
+
+        public static void GetTotal(Predicate<int> selector)
+        {
+            int total = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                if (selector(i))
+                    total += 1;
+            }
+            Console.WriteLine("Total " + total);
+        }
+    }
+}
