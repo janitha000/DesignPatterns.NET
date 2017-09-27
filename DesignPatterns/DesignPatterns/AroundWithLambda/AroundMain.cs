474bef7b99698b3ccdee46a98a9200252fc11599
Janitha Tennakoon <janitha000@gmail.com>
2017-09-27 08:28:16 +0530

    Added Around pattern with lambda

---
+++ DesignPatterns/DesignPatterns/AroundWithLambda/AroundMain.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.AroundWithLambda
+{
+    class AroundMain
+    {
+        static void Main(string[] args)
+        {
+            Database.Create(database =>
+            {
+                database.Query1();
+                database.Query2();
+            });
+
+        }
+    }
+}
---
+++ DesignPatterns/DesignPatterns/AroundWithLambda/Database.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.AroundWithLambda
+{
+    class Database
+    {
+        private Database()
+        {
+            Console.WriteLine("Database created");              
+        }
+
+        public void Query1()
+        {
+            Console.WriteLine("Query1 executed");
+        }
+
+        public void Query2()
+        {
+            Console.WriteLine("Query2 executed");
+        }
+
+        private void CleanUp()
+        {
+            Console.WriteLine("Cleanup...");        
+        }
+
+        public static void Create(Action<Database> execution)
+        {
+            Database db = new Database();
+            try
+            {
+                execution(db);
+            }
+            finally
+            {
+                db.CleanUp();
+            }
+        }
+    }
+}
